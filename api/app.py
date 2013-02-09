17995b2eb600a3fa9c6ce2a00c0d9150037778bc
Jeremia Kimelman <jbkimelman@gmail.com>
2013-02-09 15:53:24 -0800

    allowing CORS

--- api/app.py
+++ api/app.py
@@ -1,9 +1,11 @@
 from os import environ
 
-from bottle import get, run
+from bottle import get, response, run
 
 @get('/status')
 def return_status():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
     return "Up and working"
 
 run(host="0.0.0.0", port=int(environ.get("PORT", 5000)))

18a7924f7f7d4fa2fe81ece53985300c74b94615
msecret <msegreto@miceover.com>
2013-02-10 12:14:13 -0800

    Debugging label page

--- client/src/view/product_search.js
+++ client/src/view/product_search.js
@@ -38,6 +38,7 @@ R.View = R.View || {};
         data: formData,
         type: 'GET',
         success: function(data) {
+          console.log(data);
           self.model.set(data);
           R.main.trigger('product-page');
         },

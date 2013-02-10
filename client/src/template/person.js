375ff50f39b845b1240d4e9c978713e34674a1ad
msecret <msegreto@miceover.com>
2013-02-09 17:19:55 -0800

    Updated html and started first start view

--- client/src/template/person.js
+++ client/src/template/person.js
@@ -2,7 +2,14 @@
 R.Template = R.Template || {};
 
 R.Template.Person = {
-  Start: '
-
+  Start: ' \
+  <form id="personInput"> \
+    <label>Current weight</label> \
+    <input type="text" name="currentWeight" /> \
+    <label>Goal weight</label> \
+    <input type="text" name="goalWeight" /> \
+    <label>Weeks until goal</label> \
+    <input type="text" name="weeks" /> \
+  </form> \
   '
 };

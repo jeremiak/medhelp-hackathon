61d3ba33162882c6f24abb43e4ab021531ff9eb1
Jeremia Kimelman <jbkimelman@gmail.com>
2013-02-14 00:36:23 -0800

    make one massive profile setting api endpoint - sets profile with food api as well as calculates daily limit

--- api/app.py
+++ api/app.py
@@ -74,22 +74,7 @@ def search_upc():
 
     return data
 
-@get('/daily_limit')
-@post('/daily_limit')
-def calculate_daily_intake():
-    response.headers['Access-Control-Allow-Origin'] = '*'
-    
-    for param in request.query.keys():
-        print "%s: %s" % (param, request.query.get(param))
-
-    age = int(request.forms.get('age', request.query.get('age', '25')))
-    height = int(request.forms.get('height', request.query.get('height', '70')))
-    current_weight = int(request.forms.get('current_weight', request.query.get('current_weight', '180')))
-    goal_weight = int(request.forms.get('goal_weight', request.query.get('goal_weight', '170')))
-    weeks_to_goal = int(request.forms.get('weeks_to_goal', request.query.get('weeks_to_goal', '4')))
-    gender = request.forms.get('gender', request.query.get('gender', 'male')).lower()
-    activity_level = request.forms.get('activity_level', request.query.get('activity_level', 'sedentary')).lower()
-
+def calculate_daily_intake(age, height, current_weight, goal_weight, weeks_to_goal, gender, activity_level):
     pounds_per_week = (current_weight - goal_weight) / weeks_to_goal
 
     if gender == 'male':
@@ -110,51 +95,19 @@ def calculate_daily_intake():
 
     return {'limit': '%d' % limit}
 
-@get('/save_weight')
-@post('/save_weight')
-def save_weight():
-    user_id = request.query.get('user_id', None)
-    access_token = request.query.get('access_token', None)
-    weight = request.query.get('weight', None)
+@post('/set_profile')
+def set_profile():
+    response.headers['Access-Control-Allow-Origin'] = '*'
 
-    d = datetime.today()
-    date = '%s-%s-%s' % (d.year, d.month, d.day)
+    age = int(request.forms.get('age', request.query.get('age', '25')))
+    height = int(request.forms.get('height', request.query.get('height', '70')))
+    current_weight = int(request.forms.get('current_weight', request.query.get('current_weight', '180')))
+    goal_weight = int(request.forms.get('goal_weight', request.query.get('goal_weight', '170')))
+    weeks_to_goal = int(request.forms.get('weeks_to_goal', request.query.get('weeks_to_goal', '4')))
+    gender = request.forms.get('gender', request.query.get('gender', 'male')).lower()
+    activity_level = request.forms.get('activity_level', request.query.get('activity_level', 'sedentary')).lower()
 
-    url = "https://hapi.medhelp.ws/v1/users/%s/vitals?client_id=e7fc52ddd676d34660c05022e1c26fe822c4b2fe4f7555d52500007ecad5063f" % user_id
-    headers = {'Authorization': 'Token token="%s"' % access_token}
-    data = {'date': date,
-            'field_name': 'Weight',
-            'value': weight,
-            'user_id': user_id}
-
-    r = requests.post(url, headers=headers, data=json.dumps([data]))
-
-    return r.text
-
-@get('/callback')
-def handle_oauth_callback():
-    code = request.query.get('code', None)
-    url = 'https://hapi.medhelp.ws/oauth/token?client_id=e7fc52ddd676d34660c05022e1c26fe822c4b2fe4f7555d52500007ecad5063f'
-
-    data = {}
-    data['authorize'] = 'Yes'
-    data['grant_type'] = 'authorization-code'
-    data['response_type'] = 'code'
-    data['redirect_uri'] = 'http://pacific-eyrie-4115.herokuapp.com/callback'
-    data['client_id'] = 'e7fc52ddd676d34660c05022e1c26fe822c4b2fe4f7555d52500007ecad5063f'
-    data['client_secret'] = 'd7daa38353fe6e9a904630ab4e57cf56809449010b7ea0636699e60b7e84b6e7'
-    data['code'] = code
-
-    r = requests.post(url, data=data)
-    j = r.json()
-    
-    token = j['access_token']
-    user_id = j['user_id']
-    url = 'http://jeremiak.github.com/medhelp-hackathon/client/index.html#init-person?access_token=%s&user_id=%s' % (token, user_id)
-    redirect(url)
-
-@post('/set_food_profile')
-def set_food_profile():
+    daily_calorie_limit = calculate_daily_intake(age, height, current_weight, goal_weight, weeks_to_goal, gender, activity_level)
 
     true_nutrients = ['Calcium', 'Calories', 'Calories from Fat', 'Cholesterol', 'Dietary Fiber', 'Insoluble Fiber', 'Iron',
             'Monounsaturated Fat', 'Other Carbohydrate', 'Polyunsaturated Fat', 'Potassium', 'Protein', 'Saturated Fat',
@@ -238,11 +191,9 @@ def set_food_profile():
 
     params = {'sid': session_id, 'nutrients': nutrients, 'allergens': allergens, 'additives': additives, 'myingredients': [], 'mysort': []}
 
-    x = json.dumps({'json': params})
+    r = requests.post((FOOD_API % 'setprofile'), params=json.dumps({'json': params}), data={'api_key': MASHERY_KEY})
 
-    r = requests.post((FOOD_API % 'setprofile'), params=x, data={'api_key': MASHERY_KEY})
-
-    return r.status_code
+    return {'food_api_status': r.status_code, 'daily_calorie_limit': daily_calorie_limit}
 
 @get('/create_food_api_session')
 def create_session():
